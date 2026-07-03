ba0473dab636589bed6ceebadf6ca58520ea85bc
Joseph Mwangi <joseph9267mwangi@gmail.com>
2026-07-03 11:16:27 +0300

    Add import os

--- yachtsite/settings.py
+++ yachtsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,7 +119,6 @@ STATIC_URL = 'static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = False
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
 EMAIL_HOST_USER= os.environ.get('EMAIL_HOST_USER')
